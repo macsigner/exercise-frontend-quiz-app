4cae2b2f5cd27e13e391f05fe0b90f0b3ec1a423
Mac Signer <macsigner@gmx.ch>
2024-01-24 19:31:40 +0100

    feat: add base font

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,1 @@
-console.log('helloe');
+import '../scss/main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import PrettyError from 'pretty-error';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 
 const pe = new PrettyError();
 pe.skipNodeFiles();
@@ -12,17 +14,44 @@ export default {
     output: {
         path: path.resolve('dist'),
     },
+    devServer: {
+        watchFiles: ["src/pages/**/*.html"],
+        hot: true,
+    },
     module: {
         rules: [
             {
                 test: /.html$/i,
                 loader: 'html-loader',
+            },
+            {
+                test: /.(s)?css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'sass-loader',
+                    }
+                ]
             }
         ]
     },
     plugins: [
+        new MiniCssExtractPlugin(),
         new HtmlWebpackPlugin({
             template: path.resolve('src/pages/index.html'),
         }),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'src/public',
+                    to: path.resolve('dist'),
+                }
+            ]
+        })
     ]
 }
